a3609394e5f7a6bfb4895eef99b093d684106e06
joshdev <joshuasol4950@gmail.com>
2023-06-08 17:22:47 +0800

    migrate

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Navbar from "./components/Navbar.jsx";
+import AboutMe from "./components/AboutMe.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Navbar />
+    <AboutMe />
+  </React.StrictMode>
+);
