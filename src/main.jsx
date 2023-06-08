db46a847687cfb17e94b3ef1c0422bf8cd862388
joshdev <joshuasol4950@gmail.com>
2023-06-08 17:59:55 +0800

    Skills

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Navbar from "./components/Navbar.jsx";
 import AboutMe from "./components/AboutMe.jsx";
+import Background from "./components/Background.jsx";
+import Skills from "./components/Skills.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Navbar />
     <AboutMe />
+    <Background />
+    <Skills />
   </React.StrictMode>
 );
